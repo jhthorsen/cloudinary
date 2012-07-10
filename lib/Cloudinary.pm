2b7816fe64556300bd676befcb5fd5bc464761d9
Jan Henning Thorsen <jhthorsen@cpan.org>
2012-07-10 21:49:21 +0200

    0.1102        * Fix url_for(..., { type => 'facebook' });

--- lib/Cloudinary.pm
+++ lib/Cloudinary.pm
@@ -6,7 +6,7 @@ Cloudinary - Talk with cloudinary.com
 
 =head1 VERSION
 
-0.1101
+0.1102
 
 =head1 DESCRIPTION
 
@@ -90,7 +90,7 @@ use Mojo::UserAgent;
 use Mojo::Util qw/ sha1_sum url_escape /;
 use Scalar::Util 'weaken';
 
-our $VERSION = eval '0.1101';
+our $VERSION = eval '0.1102';
 our(%SHORTER, %LONGER);
 my @SIGNATURE_KEYS = qw/ callback eager format public_id tags timestamp transformation type /;
 
